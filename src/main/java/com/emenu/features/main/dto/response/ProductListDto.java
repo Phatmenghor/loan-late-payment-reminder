a4c240aca8d253a2ae059a6906ab47470b7e3ed1
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2026-02-02 15:27:12 +0700

    [development] Auto commit on 2026-02-02 15:27:11 | Files: src/main/java/com/emenu/features/main/dto/response/ProductListDto.java

--- src/main/java/com/emenu/features/main/dto/response/ProductListDto.java
+++ src/main/java/com/emenu/features/main/dto/response/ProductListDto.java
@@ -23,7 +23,7 @@ public class ProductListDto extends BaseAuditResponse {
     private LocalDateTime displayPromotionToDate;
     
     private Boolean hasSizes;
-    private Boolean hasActivePromotion;
+    private Boolean hasPromotion;
     private String mainImageUrl;
     
     private Long viewCount;
